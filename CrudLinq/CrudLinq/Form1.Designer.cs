d587eb681fae519f60121f7346f9a6e05c01233a
pedro9431 <pedro9431navarro@gmail.com>
2021-11-26 13:53:09 -0300

    Se agrega el Proyecto

---
+++ CrudLinq/CrudLinq/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace CrudLinq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtAP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAM = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvCliente = new System.Windows.Forms.DataGridView();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(130, 17);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(128, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtAP
+            // 
+            this.txtAP.Location = new System.Drawing.Point(379, 17);
+            this.txtAP.Name = "txtAP";
+            this.txtAP.Size = new System.Drawing.Size(128, 20);
+            this.txtAP.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellido Paterno";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtAM
+            // 
+            this.txtAM.Location = new System.Drawing.Point(169, 80);
+            this.txtAM.Name = "txtAM";
+            this.txtAM.Size = new System.Drawing.Size(128, 20);
+            this.txtAM.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido Materno";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(379, 80);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(128, 20);
+            this.txtTelefono.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefeno";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(218, 161);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(327, 161);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 10;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(433, 161);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(121, 224);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(204, 20);
+            this.txtBuscar.TabIndex = 13;
+            this.txtBuscar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBuscar_KeyUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Buscar";
+            // 
+            // dgvCliente
+            // 
+            this.dgvCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCliente.Location = new System.Drawing.Point(12, 268);
+            this.dgvCliente.Name = "dgvCliente";
+            this.dgvCliente.Size = new System.Drawing.Size(564, 150);
+            this.dgvCliente.TabIndex = 14;
+            this.dgvCliente.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCliente_CellClick);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(105, 161);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 15;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 433);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.dgvCliente);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LINQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtAP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAM;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvCliente;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
+
---
+++ CrudLinq/CrudLinq/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudLinq
+{
+    public partial class Form1 : Form
+    {
+
+        ConexionClienteDataContext cc = new ConexionClienteDataContext();
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ListarClientes();
+        }
+
+        void ListarClientes()
+        {
+            dgvCliente.DataSource = cc.ListarCliente();
+        }
+
+        void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtAP.Text = "";
+            txtAM.Text = "";
+            txtTelefono.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            cc.InsertarCliente(txtNombre.Text, txtAP.Text, txtAM.Text, txtTelefono.Text);
+            MessageBox.Show("Cliente guardado!");
+            this.ListarClientes();
+            this.LimpiarCampos();
+        }
+
+
+        private void dgvCliente_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                txtNombre.Text = dgvCliente.CurrentRow.Cells[1].Value.ToString();
+                txtAP.Text = dgvCliente.CurrentRow.Cells[2].Value.ToString();
+                txtAM.Text = dgvCliente.CurrentRow.Cells[3].Value.ToString();
+                txtTelefono.Text = dgvCliente.CurrentRow.Cells[4].Value.ToString();
+            }
+           
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(dgvCliente.CurrentRow.Cells[0].Value.ToString());
+            cc.ActualizarCliente(codigo,txtNombre.Text,txtAP.Text,txtAM.Text,txtTelefono.Text);
+            MessageBox.Show("Cliente actualizado!");
+            this.ListarClientes();
+            this.LimpiarCampos();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = "";
+            txtAP.Text = "";
+            txtAM.Text = "";
+            txtTelefono.Text = "";
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int codigo = Convert.ToInt32(dgvCliente.CurrentRow.Cells[0].Value.ToString());
+            cc.EliminarCliente(codigo);
+            MessageBox.Show("Cliente Eliminado!");
+            this.ListarClientes();
+            this.LimpiarCampos();
+        }
+
+        private void txtBuscar_KeyUp(object sender, KeyEventArgs e)
+        {
+            dgvCliente.DataSource = cc.BuscarCliente(txtBuscar.Text);
+        }
+    }
+}
